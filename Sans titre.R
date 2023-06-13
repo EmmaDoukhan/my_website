abc31d248eb5e72c8d2d3d45eddc26cee1b554ee
EmmaDoukhan <doukhanemma@gmail.com>
2023-06-13 11:56:05 +0100

    initial commit

---
+++ Sans titre.R
@@ -0,0 +1,11 @@
+library(blogdown) 
+blogdown::install_hugo()
+blogdown::hugo_version()
+
+blogdown::new_site(theme = "MarcusVirg/forty", 
+                   sample = TRUE, 
+                   theme_example = TRUE,            
+                   empty_dirs = TRUE,            
+                   to_yaml = TRUE)
+
+
